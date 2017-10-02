573973e8fc98db38161a59b0e045acfcc6a574a1
Brad <brad@brad.tw>
2017-10-02 15:39:05 +0800

    20171002

--- MyApp23/ViewController.swift
+++ MyApp23/ViewController.swift
@@ -7,14 +7,17 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var webView: UIWebView!
     
+    private let lmgr = CLLocationManager()
+    
     @IBAction func doTest1(_ sender: Any) {
         
-        webView.stringByEvaluatingJavaScript(from: "test1(24.155858,120.661695)")
+        webView.stringByEvaluatingJavaScript(from: "calcRoute()")
         
     }
     
@@ -22,12 +25,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        lmgr.requestAlwaysAuthorization()
+        lmgr.delegate = self
+        lmgr.startUpdatingLocation()
+        
         // read html file
         let html = Bundle.main.path(forResource: "brad", ofType: "html")
         let cont = try? String(contentsOfFile: html!)
         webView.loadHTMLString(cont!, baseURL: nil)
         
     }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
+
+        for loc in locations {
+            let lat = loc.coordinate.latitude
+            let lng = loc.coordinate.longitude
+            print("\(lat), \(lng)")
+            print("----")
+            let goJS = "test1(\(lat),\(lng))"
+            webView.stringByEvaluatingJavaScript(from: goJS)
+        }
+        print("=====")
+    }
+    
 
 }
 
